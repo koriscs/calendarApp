1f2f3b941993066d54ddcc9537c3f7035dde8776
koriscs <koriscs@gmail.com>
2022-08-19 15:44:26 +0200

    Checking Heroku ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
+require('dotenv').config();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
