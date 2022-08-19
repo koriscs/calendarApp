b6feec6dec090baa6f1a46064f24a52e48126b08
koriscs <koriscs@gmail.com>
2022-08-19 15:38:36 +0200

    Checking Heroku ready

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.get('/',(req, res) =>{
+    res.status(200).json({succes: true, message: "Succes"})
+})
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
