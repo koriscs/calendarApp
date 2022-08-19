b4390d857d751940a12941ee030bd507ec5ce8ff
koriscs <koriscs@gmail.com>
2022-08-19 17:26:38 +0200

    Making it fullstack

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 const helmet = require('helmet');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const isProduction = process.env.NODE_ENV === 'production';
-
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
 app.use(cors(origin = {origin:isProduction ? process.env.HEROKU_URL : process.env.CLIENT_URL, credentials: true }));
+app.options('*', cors(origin));
 app.use(helmet());
 
 if (process.env.NODE_ENV === "production") {
